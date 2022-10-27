a93ae04e947fa329f34ac14358f63480259822f2
Ankur Atri <ankuratri.in@gmail.com>
2022-10-27 18:10:23 +0530

    SSG with getStaticPaths example added

--- 3-pre-rendering/pages/posts/[postId].jsx
+++ 3-pre-rendering/pages/posts/[postId].jsx
@@ -11,6 +11,17 @@ function Post({ post }) {
 
 export default Post;
 
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { postId: "1" } },
+      { params: { postId: "2" } },
+      { params: { postId: "3" } },
+    ],
+    fallback: false,
+  };
+}
+
 export async function getStaticProps(context) {
   const { params } = context;
 
