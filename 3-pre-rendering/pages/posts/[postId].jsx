a6f316509371e262e328349288fa758fc795ee86
Ankur Atri <ankuratri.in@gmail.com>
2022-11-07 13:10:12 +0530

    getStaticPaths fallback true example added

--- 3-pre-rendering/pages/posts/[postId].jsx
+++ 3-pre-rendering/pages/posts/[postId].jsx
@@ -1,4 +1,12 @@
+import { useRouter } from "next/router";
+
 function Post({ post }) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
     <div>
       <h1>
@@ -15,13 +23,24 @@ export async function getStaticPaths() {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
   const data = await response.json();
 
-  const paths = data.map((post) => {
-    return { params: { postId: post.id.toString() } };
-  });
+  // fallback: false
+  // const paths = data.map((post) => {
+  //   return { params: { postId: post.id.toString() } };
+  // });
 
+  // return {
+  //   paths,
+  //   fallback: false,
+  // };
+
+  // fallback: true
   return {
-    paths,
-    fallback: false,
+    paths: [
+      { params: { postId: "1" } },
+      { params: { postId: "2" } },
+      { params: { postId: "3" } },
+    ],
+    fallback: true,
   };
 }
 
@@ -32,6 +51,14 @@ export async function getStaticProps(context) {
     `https://jsonplaceholder.typicode.com/posts/${params.postId}`
   );
   const data = await response.json();
+
+  if (!data.id) {
+    return {
+      notFound: true,
+    };
+  }
+
+  console.log(`Generating page for /posts/${params.postId}`);
 
   return {
     props: {
