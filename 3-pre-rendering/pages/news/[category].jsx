de68a84c71292ecfcc5a764bc0397119527e75d4
Ankur Atri <ankuratri.in@gmail.com>
2022-11-08 12:13:28 +0530

    getServerSideProps context parameter example added

--- 3-pre-rendering/pages/news/[category].jsx
+++ 3-pre-rendering/pages/news/[category].jsx
@@ -24,7 +24,14 @@ const ArticleListByCategory = ({ articles, category }) => {
 export default ArticleListByCategory;
 
 export async function getServerSideProps(context) {
-  const { category } = context.params;
+  const { params, req, res, query } = context;
+  console.log("query>>>", query);
+  console.log("req.headers.cookie>>>", req.headers.cookie);
+
+  res.setHeader("Set-Cookie", ["type=ninja", "language=javascript"]);
+
+  const { category } = params;
+
   const response = await fetch(
     `http://localhost:4000/news?category=${category}`
   );
