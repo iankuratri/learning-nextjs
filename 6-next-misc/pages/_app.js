f6ae9997a2198af011ae4e65c540252ff873626e
Ankur Atri <ankuratri.in@gmail.com>
2022-11-10 16:08:11 +0530

    Absolute Imports, Module Paths, Path alias example added

--- 6-next-misc/pages/_app.js
+++ 6-next-misc/pages/_app.js
@@ -1,7 +1,7 @@
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-import "../styles/globals.css";
-import "../styles/layout.css";
+import Footer from "@/layout/Footer";
+import Header from "@/layout/Header";
+import "styles/globals.css";
+import "styles/layout.css";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
