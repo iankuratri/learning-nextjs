6861587c6f39e0b5ab0e7c7d7b6c2986c393e75e
Ankur Atri <ankuratri.in@gmail.com>
2022-11-10 18:14:20 +0530

    Redirects example added

--- 6-next-misc/next.config.js
+++ 6-next-misc/next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  // redirects: async () => {
+  //   return [
+  //     {
+  //       source: "/about",
+  //       destination: "/",
+  //       permanent: false,
+  //       // permanent: true -> Status 308: permanent redirect
+  //       // permanent: false -> Status 307: temporary redirect
+  //     },
+  //     {
+  //       source: "/old-blog/:id",
+  //       destination: "/new-blog/:id",
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
