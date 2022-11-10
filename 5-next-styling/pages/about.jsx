a13775daff7ab1a7e832dde61a77bbabc1eba326
Ankur Atri <ankuratri.in@gmail.com>
2022-11-10 10:03:15 +0530

    SASS Support example added

--- 5-next-styling/pages/about.jsx
+++ 5-next-styling/pages/about.jsx
@@ -1,16 +1,25 @@
 import styles from "./../styles/About.module.css";
+import stylesScss from "./../styles/About.module.scss";
 
 function About() {
   return (
     <div>
       <h2>About Page</h2>
       <p className={styles.highlight}>
-        Highlighted Text: Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Blanditiis exercitationem necessitatibus mollitia, odit non
-        reiciendis nam vero tempore veniam qui nihil. Non, obcaecati. Quae vitae
-        delectus nisi a voluptates aliquid blanditiis accusantium beatae omnis
-        quisquam, sequi dolorum, nemo sapiente corrupti dicta tempora voluptatem
-        est explicabo adipisci animi ipsum debitis. Eveniet!
+        Highlighted Text Using CSS: Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Blanditiis exercitationem necessitatibus mollitia,
+        odit non reiciendis nam vero tempore veniam qui nihil. Non, obcaecati.
+        Quae vitae delectus nisi a voluptates aliquid blanditiis accusantium
+        beatae omnis quisquam, sequi dolorum, nemo sapiente corrupti dicta
+        tempora voluptatem est explicabo adipisci animi ipsum debitis. Eveniet!
+      </p>
+      <p className={stylesScss.highlightScss}>
+        Highlighted Text Using SCSS: Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Blanditiis exercitationem necessitatibus mollitia,
+        odit non reiciendis nam vero tempore veniam qui nihil. Non, obcaecati.
+        Quae vitae delectus nisi a voluptates aliquid blanditiis accusantium
+        beatae omnis quisquam, sequi dolorum, nemo sapiente corrupti dicta
+        tempora voluptatem est explicabo adipisci animi ipsum debitis. Eveniet!
       </p>
     </div>
   );
--- 5-next-styling/pages/contact.jsx
+++ 5-next-styling/pages/contact.jsx
@@ -1,16 +1,25 @@
 import styles from "./../styles/Contact.module.css";
+import stylesScss from "./../styles/Contact.module.scss";
 
 function Contact() {
   return (
     <div>
       <h2>Contact Page</h2>
       <p className={styles.highlight}>
-        Highlighted Text: Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Blanditiis exercitationem necessitatibus mollitia, odit non
-        reiciendis nam vero tempore veniam qui nihil. Non, obcaecati. Quae vitae
-        delectus nisi a voluptates aliquid blanditiis accusantium beatae omnis
-        quisquam, sequi dolorum, nemo sapiente corrupti dicta tempora voluptatem
-        est explicabo adipisci animi ipsum debitis. Eveniet!
+        Highlighted Text Using CSS: Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Blanditiis exercitationem necessitatibus mollitia,
+        odit non reiciendis nam vero tempore veniam qui nihil. Non, obcaecati.
+        Quae vitae delectus nisi a voluptates aliquid blanditiis accusantium
+        beatae omnis quisquam, sequi dolorum, nemo sapiente corrupti dicta
+        tempora voluptatem est explicabo adipisci animi ipsum debitis. Eveniet!
+      </p>
+      <p className={stylesScss.highlightScss}>
+        Highlighted Text Using SCSS: Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Blanditiis exercitationem necessitatibus mollitia,
+        odit non reiciendis nam vero tempore veniam qui nihil. Non, obcaecati.
+        Quae vitae delectus nisi a voluptates aliquid blanditiis accusantium
+        beatae omnis quisquam, sequi dolorum, nemo sapiente corrupti dicta
+        tempora voluptatem est explicabo adipisci animi ipsum debitis. Eveniet!
       </p>
     </div>
   );
