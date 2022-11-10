351d0a47930545cfc14fd5fe883b6acc9e616d90
Ankur Atri <ankuratri.in@gmail.com>
2022-11-10 09:50:01 +0530

    Component Level Styles (CSS Modules) example added

--- 5-next-styling/pages/about.jsx
+++ 5-next-styling/pages/about.jsx
@@ -1,5 +1,19 @@
+import styles from "./../styles/About.module.css";
+
 function About() {
-  return <h2>About Page</h2>;
+  return (
+    <div>
+      <h2>About Page</h2>
+      <p className={styles.highlight}>
+        Highlighted Text: Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Blanditiis exercitationem necessitatibus mollitia, odit non
+        reiciendis nam vero tempore veniam qui nihil. Non, obcaecati. Quae vitae
+        delectus nisi a voluptates aliquid blanditiis accusantium beatae omnis
+        quisquam, sequi dolorum, nemo sapiente corrupti dicta tempora voluptatem
+        est explicabo adipisci animi ipsum debitis. Eveniet!
+      </p>
+    </div>
+  );
 }
 
 export default About;
