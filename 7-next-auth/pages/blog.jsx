6dc766fd84a5e538ec203fdb26def91a6b0ff782
Ankur Atri <ankuratri.in@gmail.com>
2022-11-19 19:50:25 +0530

    Server-side Authentication example added

--- 7-next-auth/pages/blog.jsx
+++ 7-next-auth/pages/blog.jsx
@@ -1,5 +1,23 @@
-function Blog() {
-  return <h1>Blog Page</h1>;
+import { getSession } from "next-auth/react";
+
+function Blog({ data }) {
+  return (
+    <div>
+      <h1>Blog Page</h1>
+      <p>{data}</p>
+    </div>
+  );
 }
 
 export default Blog;
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+      data: session ? "List of 100 paid posts." : "List of 100 free posts.",
+    },
+  };
+}
